91da5acfeba2109450d5ca8a8f59f8c714318807
何小明 <460670236@qq.com>
2017-08-04 11:05:48 +0800

    第六天
    无线分级

---
+++ 1123/frame-03/index.php
@@ -0,0 +1,20 @@
+<?php
+/****
+燕十八 公益PHP讲堂
+
+论  坛: http://www.zixue.it
+微  博: http://weibo.com/Yshiba
+YY频道: 88354001
+****/
+
+
+/***
+所有由用户直接访问到的这些页面
+
+都得先加载init.php
+***/
+
+require('./include/init.php');
+
+
+print_r($_GET);
